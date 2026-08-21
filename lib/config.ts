97e71959aca14078ed4f6b7823b18b6bc9552a25
ANDRI-STORE <andrizalhacieboan@gmail.com>
2026-08-22 00:42:43 +0700

    Add configuration constants for platform limits

---
+++ lib/config.ts
@@ -0,0 +1,14 @@
+export const DAILY_LIMIT = 5;
+export const WINDOW_MS = 24 * 60 * 60 * 1000; // 24 hours
+
+export const SUPPORTED_PLATFORMS = [
+  'tiktok', 'instagram', 'soundcloud', 'spotify', 'youtube'
+];
+
+export const TIMEOUTS = {
+  tiktok: 30000,
+  instagram: 30000,
+  soundcloud: 30000,
+  spotify: 60000,
+  youtube: 90000,
+};
