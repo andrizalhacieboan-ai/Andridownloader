48f0b72ba21974f98f6e8608219d77493b172f71
ANDRI-STORE <andrizalhacieboan@gmail.com>
2026-08-22 23:57:23 +0700

    Integrate hCaptcha and Adsterra scripts
    Added hCaptcha and Adsterra scripts to the layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -16,9 +17,24 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className="dark">
       <head>
+        {/* Script hCaptcha */}
         <script src="https://js.hcaptcha.com/1/api.js" async defer></script>
       </head>
-      <body className="antialiased">{children}</body>
+      <body className="antialiased">
+        {/* Iklan Adsterra - Popunder */}
+        <Script 
+          src="https://pl30973511.profitableratecpmnetwork.com/56/20/11/56201152cd774433823838ab783b6cc1.js" 
+          strategy="afterInteractive"
+        ></Script>
+        
+        {/* Iklan Adsterra - Social Bar */}
+        <Script 
+          src="https://pl30973512.profitableratecpmnetwork.com/cd/ac/8f/cdac8f4a73fb58f8d1e2474ecbbb84a7.js" 
+          strategy="afterInteractive"
+        ></Script>
+
+        {children}
+      </body>
     </html>
   );
 }
